539db6252b6f145395e127384ef2d42461bde026
GiedriusMauza <giedrius.mauza@gmail.com>
2023-05-10 11:52:04 +0300

    addded data for testing in soap. code cleanup

--- src/main/java/lt/viko/eif/gmauza/librarymanagementwssoap/LibraryManagementWsSoapApplication.java
+++ src/main/java/lt/viko/eif/gmauza/librarymanagementwssoap/LibraryManagementWsSoapApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryManagementWsSoapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagementWsSoapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementWsSoapApplication.class, args);
+    }
 
 }
--- src/main/java/lt/viko/eif/gmauza/librarymanagementwssoap/LibraryRepository.java
+++ src/main/java/lt/viko/eif/gmauza/librarymanagementwssoap/LibraryRepository.java
@@ -4,10 +4,10 @@ import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
-
-import lt.viko.eif.librarymanagementwssoap.Library;
+import lt.viko.eif.librarymanagementwssoap.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+
 @Component
 public class LibraryRepository {
 
@@ -15,9 +15,56 @@ public class LibraryRepository {
 
     @PostConstruct
     public void initData() {
+
+        Item item = new Item();
+        item.setAuthor("Edmundas Untulis");
+        item.setTitle("Žvilgsnis iš maišo");
+        item.setPublisher("2023");
+        item.setProductionYear("Eglės leidykla");
+
+        Borrowing borrowing = new Borrowing();
+        borrowing.setBorrowDate("2023-01-05");
+        borrowing.setReturnDate("2023-06-25");
+        borrowing.setItem(item);
+
+        Item item2 = new Item();
+        item2.setAuthor("Arnoldas Jonulis");
+        item2.setTitle("Žvilgsnis pro langą");
+        item2.setPublisher("2022");
+        item2.setProductionYear("Eridanas leidykla");
+
+        Borrowing borrowing2 = new Borrowing();
+        borrowing2.setBorrowDate("2023-02-08");
+        borrowing2.setReturnDate("2023-05-15");
+        borrowing2.setItem(item2);
+
+        Borrowings borrowings = new Borrowings();
+        borrowings.getBorrowing().add(borrowing);
+        borrowings.getBorrowing().add(borrowing2);
+
+        Subscriber subscriber = new Subscriber();
+        subscriber.setSubscriberName("Benas");
+        subscriber.setSubscriberLastName("Antanaitis");
+        subscriber.setAddress("Kalno g. 66");
+        subscriber.setEmail("benas@test.ff");
+        subscriber.setPhone("+3705465654");
+        subscriber.setBorrowings(borrowings);
+
+        Subscribers subscribers = new Subscribers();
+        subscribers.getSubscriber().add(subscriber);
+
+        Librarian librarian = new Librarian();
+        librarian.setLibrarianName("Jonas");
+        librarian.setLastName("Petraitis");
+        librarian.setLibrarianEmail("test@mail.ff");
+        librarian.setPhoneNumber("+3706645654");
+
         Library central = new Library();
         central.setLibraryName("Central Library");
         central.setLibraryAddress("Gedimino pr.32");
+        central.setWorkHours("09h.-18h.");
+        central.setLibrarian(librarian);
+        central.setSubscribers(subscribers);
 
         libraries.put(central.getLibraryName(), central);
 
